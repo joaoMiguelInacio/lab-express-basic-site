45dd73231939d09e2993c163c012a448007e0cab
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-04-25 09:49:19 +0100

    nav bar animation added

---
+++ public/js/nav-detail.js
@@ -0,0 +1,15 @@
+if (window.location.pathname === "/"){
+    document.getElementById("home").classList.add("current-menu-item");
+}
+
+if (window.location.pathname === "/about"){
+    document.getElementById("about").classList.add("current-menu-item");
+}
+
+if (window.location.pathname === "/episode-guide"){
+    document.getElementById("episode-guide").classList.add("current-menu-item");
+}
+
+if (window.location.pathname === "/gallery"){
+    document.getElementById("gallery").classList.add("current-menu-item");
+}
